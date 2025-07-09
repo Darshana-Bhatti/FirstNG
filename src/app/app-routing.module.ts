b333413e3168a95e15f2adc5e940c01d2fe4a81d
Darshana-Bhatti <darshj2210@gmail.com>
2025-07-09 18:21:28 +0530

    wordcounter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { HomeComponent } from './home/home.component';
+import { WordCounterComponent } from './word-counter/word-counter.component';
 
 const routes: Routes = [
   {path:"",component:HomeComponent},
   {path:"todo",component:TodoListComponent},
+  {path:'wordC',component:WordCounterComponent},
   {path:"**", redirectTo:""}
 ];
 
--- src/app/word-counter/word-counter.component.ts
+++ src/app/word-counter/word-counter.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./word-counter.component.css']
 })
 export class WordCounterComponent {
+textInput: string = '';
 
+getWordCount(): number {
+  return this.textInput.trim() ? this.textInput.trim().split(/\s+/).length : 0;
+}
+
+getCharCount(): number {
+  return this.textInput.length;
+}
+
+getCharCountWithoutSpace(): number {
+  return this.textInput.replace(/\s/g, '').length;
+}
+
+getSentenceCount(): number {
+  return this.textInput.split(/[.!?]+/).filter(s => s.trim().length > 0).length;
+}  
 }
